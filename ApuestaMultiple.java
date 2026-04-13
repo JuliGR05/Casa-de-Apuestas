b4f2dbe526fde896a0b253090cbf8e6fc1b760ae
Juli <julianita.gutierrez2005@gmail.com>
2026-04-12 19:21:48 -0500

    Creación clase ApuestaMultiple

---
+++ ApuestaMultiple.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class ApuestaMultiple extends Apuesta {
+
+    private List<LineaApuesta> lineas;
+
+    // Constructor
+    public ApuestaMultiple(double monto, Jugador jugador, List<LineaApuesta> lineas) {
+        super(monto, jugador);
+        this.lineas = lineas;
+    }
+
+    // Método para calcular ganancia
+    @Override
+    public double calcularGananciaPotencial(double monto) {
+        double producto = 1;
+
+        for (LineaApuesta linea : lineas) {
+            producto *= linea.getCuotaSeleccionada();
+        }
+
+        return monto * producto;
+    }
+}
